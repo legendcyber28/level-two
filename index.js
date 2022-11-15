0a9c6c092aab6c6e5301f5408d28e6de895c897a
legendcyber28 <108624644+legendcyber28@users.noreply.github.com>
2022-11-15 15:14:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const http = require("http");
+const fs = require("fs");
+// const args = require('minimist')(process.argv.slice(2));
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+let registrationStyle = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+fs.readFile("registrationStyling.js", (err, style) => {
+
+  if (err)
+   {
+    throw err;
+  }
+  registrationStyle = style;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      
+        case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      
+        case "/style":
+        response.write(registrationStyle);
+        response.end();
+        break;
+      
+        default:
+        response.write(homeContent);
+        response.end();
+      break;
+    }
+  // }).listen(args.port);
+}).listen(3000);
